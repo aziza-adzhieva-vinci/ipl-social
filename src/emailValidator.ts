a392d4c4f0a8e6edd3d057fe990d4d198a8016d1
Aziza Adzhieva <adzaa@mac.home>
2026-01-16 16:44:34 +0100

    fix feature validator

--- src/emailValidator.ts
+++ src/emailValidator.ts
@@ -1,3 +1,18 @@
-export function isValidEmail(_email: string): boolean {
-  return false;
+export function isValidEmail(email: string): boolean {
+  
+  if (email.includes(" ")) return false;
+
+  const atIndex = email.indexOf("@");
+  if (atIndex === -1) return false;
+
+  
+  const before = email.slice(0, atIndex);
+  const after = email.slice(atIndex + 1);
+  if (before.length === 0 || after.length === 0) return false;
+
+  
+  if (!after.includes(".")) return false;
+  if (after.endsWith(".")) return false;
+
+  return true;
 }
